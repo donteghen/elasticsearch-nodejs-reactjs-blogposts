2371ca76cd82deb2d507df2a84ecd9edfb8912e1
donteghen <donaldteghen@gmail.com>
2022-09-18 22:35:00 +0100

    Completed initial setup and implementation of a nodejs, elasticsearch and reactjs based post page

---
+++ backend/create-index.js
@@ -0,0 +1,10 @@
+// backend/create-index.js
+
+const elasticClient = require("./elastic-client");
+
+const createIndex = async (indexName) => {
+  await elasticClient.indices.create({ index: indexName });
+  console.log("Index created");
+};
+
+createIndex("posts");
---
+++ backend/index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const elasticClient = require("./elastic-client");
+require("express-async-errors");
+require("dotenv").config({ path: ".elastic.env" });
+const app = express();
+
+app.use(bodyParser.json());
+
+
+  app.post("/create-post", async (req, res) => {
+    const result = await elasticClient.index({
+      index: "posts",
+      document: {
+        title: req.body.title,
+        author: req.body.author,
+        content: req.body.content,
+      },
+    });
+  
+    res.send(result);
+  });
+
+    app.delete("/remove-post", async (req, res) => {
+    const result = await elasticClient.delete({
+        index: "posts",
+        id: req.query.id,
+    });
+
+        res.json(result);
+    });
+    app.get("/search", async (req, res) => {
+        const result = await elasticClient.search({
+            index: "posts",
+            query: { fuzzy: { title: req.query.query } },
+        });
+
+        res.json(result);
+    });
+
+    app.get("/posts", async (req, res) => {
+        const result = await elasticClient.search({
+            index: "posts",
+            query: { match_all: {} },
+        });
+
+        res.send(result);
+    });
+
+    app.get("/", (req, res) => {
+        res.redirect("http://localhost:3000/");
+    });
+
+app.listen(8080);
